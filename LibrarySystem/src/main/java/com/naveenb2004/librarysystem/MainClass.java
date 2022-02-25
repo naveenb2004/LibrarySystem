130b8ce874b647febbeca7eb4d294d2a9c181e82
naveenb2004 <naveennbalasooriya2004@gmail.com>
2022-02-25 22:40:12 +0530

    opend project

---
+++ LibrarySystem/src/main/java/com/naveenb2004/librarysystem/MainClass.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.naveenb2004.librarysystem;
+
+/**
+ *
+ * @author Naveen Balasooriya
+ */
+public class MainClass {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MainInterface maininterface = new MainInterface();
+        maininterface.setVisible(true);
+        
+    }
+    
+}
